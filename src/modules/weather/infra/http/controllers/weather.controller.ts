a4c40778fcc8dfae0b4c64c15e1f528f665459a0
Leonardo-teodoro <71471662+Leonardo-teodoro@users.noreply.github.com>
2022-08-10 19:02:44 -0300

    Add header to deal with CORS policy

--- src/modules/weather/infra/http/controllers/weather.controller.ts
+++ src/modules/weather/infra/http/controllers/weather.controller.ts
@@ -1,6 +1,5 @@
-import { HttpService } from '@nestjs/axios';
 import { IWeatherDTO } from '../../../dtos/IWeatherDataDTO';
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Header, Query } from '@nestjs/common';
 import { WeatherService } from '@modules/weather/services/weather.service';
 
 @Controller()
@@ -8,7 +7,8 @@ export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
   @Get('cities/temperature')
-  findAndCountCitiesTemperature(@Query() query: IWeatherDTO): {} {
+  @Header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  findAndCountCitiesTemperature(@Query() query: IWeatherDTO) {
     return this.weatherService.getCitiesTemperature(query);
   }
 }
